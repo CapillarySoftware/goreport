2776255fe87ea56aeaa97d89fde33ef99c883714
Ben Aldrich <vrecan@gmail.com>
2014-08-16 00:13:10 -0600

    fix tests

--- goreporter_test.go
+++ goreporter_test.go
@@ -6,12 +6,21 @@ import (
 )
 
 var _ = gi.Describe("Goreporter", func() {
-	gi.It("Basic creation", func() {
+	gi.It("Basic creation with invalid queue name", func() {
+		url := "fail"
+		r, err := NewReporter(&url)
+		r.SetTimeout(1)
+		gom.Expect(err).ShouldNot(gom.Equal(gom.BeNil()))
+	})
+
+	gi.It("Basic new stat with failed flush", func() {
 		url := "ipc:///tmp/test"
-		rep, err := NewReporter(&url)
+		r, err := NewReporter(&url)
+		r.SetTimeout(1)
 		gom.Expect(err).Should(gom.BeNil())
 		key := "key"
-		rep.AddStat(key, 30)
-		rep.Flush()
+		r.AddStat(key, 30)
+		err = r.Flush()
+		gom.Expect(err).ShouldNot(gom.Equal(gom.BeNil()))
 	})
 })
