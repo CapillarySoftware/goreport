2c7708977a0881d73409232c65c9f5dde327bff3
Ben Aldrich <vrecan@gmail.com>
2014-08-16 21:12:31 -0600

    first working background thread stat reporter

--- goreporter.go
+++ goreporter.go
@@ -4,17 +4,78 @@ import (
 	"github.com/CapillarySoftware/gostat/protoStat"
 	nano "github.com/op/go-nanomsg"
 	// "strings"
-	// "fmt"
+	"fmt"
+	"sync"
 	"time"
 )
 
-type Reporter struct {
-	socket *nano.PushSocket
-	stats  map[string]float64
+var asyncQ chan *protoStat.ProtoStat
+var wg sync.WaitGroup
+
+func init() {
+	wg.Add(1)
+	asyncQ = make(chan *protoStat.ProtoStat, 1000)
+	go asyncProcess(asyncQ)
 }
 
+type Reporter struct {
+	socket *nano.PushSocket
+	async  chan *protoStat.ProtoStat
+}
+
+//async background thread that processing all stats
+func asyncProcess(q <-chan *protoStat.ProtoStat) {
+	stats := make(map[string]*protoStat.ProtoStat)
+	reportInterval := make(chan bool, 1)
+	go func() {
+		for {
+			time.Sleep(5 * time.Second)
+			reportInterval <- true
+		}
+	}()
+main:
+	for {
+		select {
+		case m := <-q:
+			if nil == m {
+				break main
+			}
+			updateMap(stats, m)
+			fmt.Println(stats)
+		case report := <-reportInterval:
+			fmt.Println("Time to report :", report)
+		}
+	}
+
+	//cleanup anything still on the queue
+	for m := range q {
+		if nil == m {
+			break
+		}
+		updateMap(stats, m)
+	}
+
+	fmt.Println("Finished bg thread")
+	wg.Done()
+}
+
+//update map with new data
+func updateMap(stats map[string]*protoStat.ProtoStat, stat *protoStat.ProtoStat) {
+	// fmt.Println("map: ", stats)
+	ck := stat.GetKey() + stat.GetIndexKey()
+	oldStat, ok := stats[ck]
+	if !ok {
+		stats[ck] = stat
+	} else {
+		v := oldStat.GetValue() + stat.GetValue()
+		oldStat.Value = &v
+	}
+}
+
+//New reporter that reports when flushed
 func NewReporter(url *string) (r Reporter, err error) {
-	r = Reporter{stats: make(map[string]float64)}
+
+	r = Reporter{async: asyncQ}
 	err = r.connect(url)
 	r.SetTimeout(0)
 	return
@@ -36,24 +97,32 @@ func (this *Reporter) SetTimeout(millis time.Duration) {
 }
 
 func (this *Reporter) AddStat(key string, value float64) {
-	this.stats[key] += value
+	stat := protoStat.ProtoStat{Key: &key, Value: &value}
+	this.async <- &stat
 }
 
-func (this *Reporter) AddStatWIndex(key string, value float64, indexName string) {
-	s := key + indexName
-	this.stats[s] += value
+//Add multiple stats into the same graph
+func (this *Reporter) AddStatWIndex(key string, value float64, indexKey string) {
+	stat := protoStat.ProtoStat{Key: &key, Value: &value, IndexKey: &indexKey}
+	this.async <- &stat
 }
 
-//Send data to queue
-func (this *Reporter) Flush() (err error) {
-	var s []*protoStat.ProtoStat
-	stats := new(protoStat.ProtoStats)
-	for k, v := range this.stats {
-		stat := protoStat.ProtoStat{Key: &k, Value: &v}
-		s = append(s, &stat)
-	}
-	stats.Stats = s
-	bytes, err := stats.Marshal()
-	_, err = this.socket.Send(bytes, 0) //blocking
-	return
+func (this *Reporter) Close() {
+	close(this.async)
+	fmt.Println("Closed queue, waiting for cleanup")
+	wg.Wait()
 }
+
+// //Send data to queue
+// func (this *Reporter) Flush() (err error) {
+// 	var s []*protoStat.ProtoStat
+// 	stats := new(protoStat.ProtoStats)
+// 	for k, v := range this.stats {
+// 		stat := protoStat.ProtoStat{Key: &k, Value: &v}
+// 		s = append(s, &stat)
+// 	}
+// 	stats.Stats = s
+// 	bytes, err := stats.Marshal()
+// 	_, err = this.socket.Send(bytes, 0) //blocking
+// 	return
+// }
--- goreporter_test.go
+++ goreporter_test.go
@@ -1,6 +1,8 @@
 package goreporter
 
 import (
+	"fmt"
+	"github.com/CapillarySoftware/gostat/protoStat"
 	gi "github.com/onsi/ginkgo"
 	gom "github.com/onsi/gomega"
 )
@@ -15,12 +17,42 @@ var _ = gi.Describe("Goreporter", func() {
 	gi.It("Basic new stat with failed flush", func() {
 		url := "ipc:///tmp/test"
 		r, err := NewReporter(&url)
+		defer r.Close()
 		gom.Expect(err).Should(gom.BeNil())
 		key := "key"
 		r.AddStat(key, 30)
 		r.AddStatWIndex(key, 30, "index")
-		err = r.Flush()
+
 		gom.Expect(err).ShouldNot(gom.Equal(gom.BeNil()))
+	})
+
+	gi.It("Validate update map increments correctly with indexKeys", func() {
+		stats := make(map[string]*protoStat.ProtoStat)
+		key := "key"
+		indexKey := "index"
+		value := float64(200)
+		for i := 0; i < 2; i++ {
+			stat := protoStat.ProtoStat{Key: &key, Value: &value, IndexKey: &indexKey}
+			updateMap(stats, &stat)
+		}
+		gom.Expect(len(stats)).Should(gom.Equal(1))
+		for _, s := range stats {
+			gom.Expect(s.GetValue()).Should(gom.Equal(float64(400)))
+		}
+	})
+
+	gi.It("Validate update map increments correctly with standard key value pairs", func() {
+		stats := make(map[string]*protoStat.ProtoStat)
+		key := "key"
+		value := float64(200)
+		for i := 0; i < 2; i++ {
+			stat := protoStat.ProtoStat{Key: &key, Value: &value}
+			updateMap(stats, &stat)
+		}
+		gom.Expect(len(stats)).Should(gom.Equal(1))
+		for _, s := range stats {
+			gom.Expect(s.GetValue()).Should(gom.Equal(float64(400)))
+		}
 	})
 
 })
